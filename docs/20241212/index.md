fb83fc92181bbb22dec7b12c36ea1268c34237a0
magicat888 <magicat888@gmail.com>
2024-12-08 22:28:32 +0800

    new commit - add files to the repo

---
+++ docs/20241212/index.md
@@ -0,0 +1,7 @@
+# Hong Kong Philharmonic 20241212
+
+## **Lio Kuokman & Stephen Hough**
+
+- Fazıl SAY - Grand Bazaar
+- SAINT-SAËNS - Piano Concerto no. 5, Egyptian
+- BERLIOZ - Symphonie fantastique
---
+++ init-repo.ps1
@@ -0,0 +1,20 @@
+# Create a README.md file with initial content  
+echo "# HKPO202425" > README.md  
+
+# Initialize a new Git repository  
+git init  
+
+# Add README.md to the staging area  
+git add README.md  
+
+# Commit the changes with a message  
+git commit -m "first commit"  
+
+# Rename the branch to 'main'  
+git branch -M main  
+
+# Add the remote repository  
+git remote add origin https://github.com/magicat888/HKPO202425.git  
+
+# Push the changes to the remote repository  
+git push -u origin main
