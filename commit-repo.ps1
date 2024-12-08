512f86c3a72334d59c706fde205170959a388b8e
magicat888 <magicat888@gmail.com>
2024-12-08 22:47:11 +0800

    new commit - add files to the repo

--- commit-repo.ps1
+++ commit-repo.ps1
@@ -4,8 +4,5 @@ git add .
 # Commit the changes with a message  
 git commit -m "new commit - add files to the repo"  
 
-# Add the remote repository  
-git remote add origin https://github.com/magicat888/HKPO202425.git  
-
 # Push the changes to the remote repository  
 git push -u origin main
